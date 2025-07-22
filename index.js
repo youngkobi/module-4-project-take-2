0f54b401a37cb1dd718127a3975fe458dc27add4
Jakobi <jahardy2021@gmail.com>
2025-07-22 10:16:33 -0500

    tweaks

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 const movieSearchBox = document.getElementById('movie-search-box')
 const bookswrapper =document.querySelector(".books")
+const movieListEl = document.querySelector(".user-list")
+const searchTitle = document.getElementById("search__title")
+let movie = []
+
+
 
 function filtermovies(filter){
       if(filter === "Old_To_New"){
-        movie.sort((a,b)=> a.year - b.year)
+        movie.sort((a,b)=> parseInt(a.year) - parseInt(b.year))
       }
       else if(filter === "New_to_Old"){
-        movie.sort((a,b)=> a.year - b.year)
+        movie.sort((a,b)=> parseInt(b.year) - parseInt(a.year))
       }
 
 }
+
 
 
 
@@ -22,40 +28,44 @@ bookswrapper.classList.add("books__loading")
     const res = await fetch (`${URL}`)
     const data = await res.json()
     bookswrapper.classList.remove('books__loading')
-    const movieListEl = document.querySelector(".user-list")
+    
 
-    filtermovies()
-
-
-    movieListEl.innerHTML = 
-    data.Search.map((movie) => `
-    <div class="user-card">
-            <div class="user-card__container">
-              <h3>${movie.Title}</h4>
-                <p><b>Year:</b> ${movie.Year}</p>
-                  <img class="poster__img" src="${movie.Poster}" alt="" >
-            </div>
-          </div>`
-    ).slice(0, 6)
-.join("")
-
-
-// console.log(data.Search)
-    // if(data.response = 'true') displayMovieList(data.Search)
+    if (data.Response === true){
+      movie = data.Search
+      renderMovies()
+    }
+    else{
+      movieListEl.innerHTML = `<p> No Movies Found </p>`
+    }
+      
 }
+
+
+function renderMovies(){
+ movieListEl.innerHTML = 
+       movie.map((movieDetail) => `
+       <div class="user-card">
+       <div class="user-card__container">
+       <h3>${movieDetail.Title}</h4>
+       <p><b>Year:</b> ${movieDetail.Year}</p>
+       <img class="poster__img" src="${movieDetail.Poster}" alt="" >
+       </div>
+       </div>`
+      ).slice(0, 6)
+      .join("")
+}
+
+
 
 function findMovies(){
     let searchTerm = (movieSearchBox.value).trim()
     loadMovies(searchTerm)
-     const searchTitle = document.getElementById("search__title")
   searchTitle.innerHTML = `<h2> Search Results for: ${searchTerm}<h2></h2>`
   console.log(movieSearchBox.value) 
     }
 
 
-
-    
-
     function filterBooks(event) {
-      loadMovies(event.target.value)
+      const selectedFilter = event.target.value
+      filtermovies(selectedFilter)
     }
